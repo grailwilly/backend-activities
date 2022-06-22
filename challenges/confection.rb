46d58289867f902faeb681f44006d023c5646a20
grailwilly <grailjwi11y@gmail.com>
2022-06-22 23:06:54 +0800

    Add inheritance class in confection file

--- challenges/confection.rb
+++ challenges/confection.rb
@@ -3,3 +3,28 @@
 # also need to be frosted afterward. Write a prepare method for your Cupcake and Banana Cake class that prints 
 # "Baking at 350 degrees for 25 minutes, and then prints "Applying frosting" for Cupcakes only
 
+class Confection
+  def prepare
+    "Baking at 350 degrees for 25 minutes"
+  end
+
+  def frost
+    "Applying frosting"
+  end
+end
+
+class Cupcakes < Confection
+end
+
+class Banana_Cakes < Confection
+end
+
+cupcake = Cupcakes.new()
+puts cupcake.prepare
+puts cupcake.frost
+
+banana_cake = Banana_Cakes.new()
+puts banana_cake.prepare
+
+
+
