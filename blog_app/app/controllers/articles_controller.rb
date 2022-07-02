77be5997b71f112ff58608b099195346f28e2cbd
grailwilly <grailjwi11y@gmail.com>
2022-07-02 12:49:49 +0800

    Modify format validations in article

--- blog_app/app/controllers/articles_controller.rb
+++ blog_app/app/controllers/articles_controller.rb
@@ -10,11 +10,19 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    if @article.save
-      redirect_to articles_path
-    else
-      render :new
+    respond_to do |format|
+      if @article.save
+        format.html {redirect_to articles_path, notice: "Successfully create entry" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
+
+    # if @article.save
+    #   redirect_to articles_path
+    # else
+    #   render :new
+    # end
   end
 
   def show
