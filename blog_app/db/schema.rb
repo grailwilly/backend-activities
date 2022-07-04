d1dc203d40dbf0f619492ecf67e5c59584b22207
grailwilly <grailjwi11y@gmail.com>
2022-07-04 20:58:15 +0800

    Generate Comment model association with Article

--- blog_app/db/schema.rb
+++ blog_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_02_025551) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_04_123301) do
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.text "body"
@@ -30,6 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_02_025551) do
     t.string "name"
     t.text "description"
     t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
